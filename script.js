ced07fcf46ddbe5b4a7c09b7129031bb1557b5a2
Krn2706 <kathrine@asimba.dk>
2025-02-10 15:28:36 +0000

    PWA

---
+++ script.js
@@ -0,0 +1,20 @@
+// Arrays med navne-dele 
+const firstPart = [
+  "Skatte", "Fnuller", "Putte", "Guld", "Musse", "Snuske", "Mælke", "Jelly", "Cookie", "Buller", 
+  "Kramme", "Guld", "Prutte", "Misse", "Stjerne", "Tosse", "Grine", "Puller", "Sveske", "Mulle", "Tulle", "Smukke", "Sukker", "Lækker", "Sol", "Prinsesse", "Elske", "Charme", "Lækker", "Honning", "Nusse", "Skum", "Mini", "Prinse", "Sommer", "Puslinge", "Kramme", "Nuller", "Bølle", "Muller", "Pjuske", "Plysser", "Tutte", "Smør", "Fjolle", "Glitter", "Snurre", "Bobbel", "Plys", "Lækker"
+];
+
+const secondPart = [
+  "mus", "gøj", "-tøs", "bamse", "-fis", "mis", "-bøtte", "bean", "monster", "mås", 
+  "bjørn", "klump", "top", "dyr", "kat", "lys", "tosse", "-fyr", "dukke", "bisken", "trold", "sikke", "-futmælk", "ling", "kage", "-fidus", "-bar", "panda", "-muuh", "fugl", "boble", "blomst", "stjerne", "fisk", "gøjl", "stråle", "my", "-top", "fnug", "sky", "blomst", "fe", "ælling", "hvalp", "tumling", "-smølf", "perle", "pjusk", "drys", "putling", "-puff"
+];
+
+// Funktion til at generere et tilfældigt kælenavn
+function generateNickname() {
+  const randomFirst = firstPart[Math.floor(Math.random() * firstPart.length)];
+  const randomSecond = secondPart[Math.floor(Math.random() * secondPart.length)];
+  document.getElementById("nickname").innerText = randomFirst + randomSecond;  // Sammenføje uden mellemrum
+}
+
+// Håndter knap klik
+document.getElementById("generate").addEventListener("click", generateNickname);
---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "nickname-pwa-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./script.js",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+// Installer Service Worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Aktiver og opdater cache
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Håndter fetch requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
